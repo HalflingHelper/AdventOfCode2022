8c3654cd363567576fe5f411105d6c323e2c87f5
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2022-12-02 16:29:21 -0500

    Days 1 and 2

--- src/Day1.java
+++ src/Day1.java
@@ -1,10 +1,38 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Day1 {
-    public static void main(String[] args) throws java.io.FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
+        part1();
+        part2();
+    }
+
+    public static void part1() throws FileNotFoundException {
+        File input = new File("input.txt");
+        Scanner in = new Scanner(input);
+
+        int cur = 0;
+        int max = 0;
+
+
+        while (in.hasNextLine()) {
+            String s = in.nextLine();
+            if (s.equals("")) {
+                max = Math.max(cur, max);
+                cur = 0;
+            } else {
+                int i = Integer.parseInt(s);
+                cur += i;
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    public static void part2() throws FileNotFoundException {
         File input = new File("input.txt");
         Scanner in = new Scanner(input);
 
--- src/Day2.java
+++ src/Day2.java
@@ -4,6 +4,49 @@ import java.util.Scanner;
 
 public class Day2 {
     public static void main(String[] args) throws FileNotFoundException {
+        part1();
+        part2();
+    }
+
+    public static void part1() throws FileNotFoundException {
+        File input = new File("input2.txt");
+        Scanner in = new Scanner(input);
+
+        int score = 0;
+
+        while (in.hasNextLine()) {
+            String l = in.nextLine();
+            char b = l.charAt(0);
+            char a   = l.charAt(2);
+
+            if (a == 'X') { //Rock
+                score += 1;
+                if (b == 'A') { //Draw to rock
+                    score += 3;
+                }  else if (b == 'C') { //Lose to Scissors
+                    score += 6;
+                }
+
+            } else if (a == 'Y') { //Paper
+                score += 2;
+                if (b == 'A') { //Beats rock
+                    score += 6;
+                } else if (b == 'B') { //Draws paper
+                    score += 3;
+                }
+            } else if (a == 'Z') { //Scissors
+                score += 3;
+                if (b == 'C') { //Draw scissors
+                    score += 3;
+                } else if (b == 'B') { //Beat paper
+                    score += 6;
+                }
+            }
+        }
+
+        System.out.println(score);
+    }
+    public static void part2() throws FileNotFoundException {
         File input = new File("input2.txt");
         Scanner in = new Scanner(input);
 
